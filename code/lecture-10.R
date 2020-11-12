dbbe535236fbce91737c095841e0bbf17ee772e6
Marcy Shieh <mshieh2@wisc.edu>
2020-11-12 09:27:12 -0800

    post-lecture 10 update

--- code/lecture-10.R
+++ code/lecture-10.R
@@ -14,6 +14,8 @@ library("sjlabelled")
 here("data")
 bork <- read_dta(here("data", "Bork.dta")) # download from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/W5CV8E&version=1.0
 
+View(bork)
+
 # table 1: descriptive stats - mean, deviation, min, max
 # senate vote (vote), lack of qualifications (lackqual), ideological distance (eucldist), strong president (strngprs), same party (sameprty)
 # https://vincentarelbundock.github.io/modelsummary/articles/datasummary.html
@@ -35,7 +37,7 @@ table2_ccs_nominees_model <- glm(vote ~ lackqual + eucldist + strngprs + sameprt
                            data = table2_ccs_nominees)
 summary(table2_ccs_nominees_model)
 
-# time frame error in the paper: it claims this is the time frame from black to black to roberts but it seems to be the time frame between black to alito (did not realize they also included alito in their analysis lol)
+# time frame error in the paper: it claims this is the time frame from black to roberts but it seems to be the time frame between black to alito (did not realize they also included alito in their analysis lol)
 table2_add_nominees <- glm(vote ~ lackqual + eucldist + strngprs + sameprty,
                  family = binomial(link = "probit"), 
                  data = bork)
@@ -63,6 +65,8 @@ poole$name <- trimws(poole$name)
 # rename congress
 poole <- rename(poole, congress = cong)
 
+View(poole)
+
 # say you want to look at additional nominees
 # extension: from black to alito
 
@@ -72,6 +76,8 @@ poole <- rename(poole, congress = cong)
 
 # download all the roll call votes of the 109 congress
 congress109 <- read_dta(here("data", "sen109kh.dta")) 
+
+View(congress109)
 
 # identify which column contains the alito votes by downloading the dictionary
 # it seems like V368 is the alito vote column
@@ -115,7 +121,7 @@ alito <- congress109_alito %>%
 
 # select relevant variables in the poole & rosenthal common space scores
 poole_select <- poole %>%
-  select(congress, idno, statenm, name, dwnom1)
+  select(congress, idno, statenm, name, dwnom1, party)
 
 # merge the senators' alito votes with the senators' common space scores
 alito_poole <- left_join(alito, poole_select, by=c("id" = "idno", "congress", "name"))
@@ -123,16 +129,25 @@ names(alito_poole)
 alito_poole <- rename(alito_poole, cs1 = dwnom1)
 # now you want to build a dataframe with the following information:
 # nominee, presprty, sameprty, strngprs, nomid, regime_dummy, lackqual, csnom, eucldist, interaction
-alito_poole$nominee <- rep("ALITO")
-alito_poole$presprty <- rep(1)
-alito_poole$sameprty <- rep(1)
-alito_poole$nomid <- rep(0.100) # https://en.wikipedia.org/wiki/Segal%E2%80%93Cover_score
-alito_poole$regime_dummy <- rep(1)
-alito_poole$lackqual <- rep(1-0.810) #https://en.wikipedia.org/wiki/Segal%E2%80%93Cover_score
+alito_poole$nominee <- rep("ALITO") # name of nominee
+alito_poole$presprty <- rep(1) # president held party majority in Senate
+
+# whether senator is the same party as the president
+alito_poole %>%
+  mutate(sameprty = ifelse(party == 200,
+                           1,
+                           0))
+
+alito_poole$nomid <- rep(0.100) # ideology score of the judicial nominee: https://en.wikipedia.org/wiki/Segal%E2%80%93Cover_score
+alito_poole$regime_dummy <- rep(1) # whether the judicial nominee was pre- or post-bork (1 is post-bork, 0 is pre-bork)
+alito_poole$lackqual <- rep(1-0.810) # you take how qualified someone is based on newspaper editorials and subtract it by 1 to get their "lack of qualifications" score #https://en.wikipedia.org/wiki/Segal%E2%80%93Cover_score
+
 # you will need to calculate the following (later)
-alito_poole$csnom <- NA
-alito_poole$eucldist <- NA
-alito_poole$interaction <-NA
+alito_poole$csnom <- NA # this is going to be the Segal-Cover score for each nominee "transformed" as the Common Space/Poole & Rosenthal/DW-Nominate (it goes by many names) score
+
+alito_poole$eucldist <- NA # this is going to be the Euclidean distance between the senator and the nominee
+
+alito_poole$interaction <-NA # this is the interaction between ideological distance and qualifications
   
 # bind the dataset to the bork dataset
 bork_alito_poole <- full_join(bork, alito_poole)
@@ -141,6 +156,7 @@ names(bork_alito_poole)
 # you can just keep them there if they don't interfere with your analysis going forward
 
 # from reading the article, you know that you need the common space scores for each president as well
+# the president is coded with a congressional district (cd) number of 0, and a state number (state) of 99
 poole_filter <- poole %>%
   filter(cd == 0 & state == 99) %>%
   select(congress, name, dwnom1)
@@ -165,6 +181,9 @@ bork_alito_poole_full$cnom_alito <- 0.5816398 - 0.9868674*bork_alito_poole_full$
 
 # do the calculations for ideological distance
 bork_alito_poole_full$eucldist_alito <- (bork_alito_poole_full$cnom_alito - bork_alito_poole_full$cs1)^2
+
+# do the calculations for the interaction between ideological distance and qualifications
+bork_alito_poole_full$interaction_alito <- bork_alito_poole_full$eucldist_alito*alito_poole$lackqual
 
 View(bork_alito_poole_full)
 
